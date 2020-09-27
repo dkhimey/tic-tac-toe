033cfb9482fc9d408e319fa92e586f04933c8e5f
Diana Khimey <dianakhimey@Dianas-MacBook-Pro.local>
2020-09-26 23:05:34 -0400

    Working Game!

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # tic-tac-toe
 
-Play a game against an AI by downloading code, and running 'python runner.py' in the folder.
+Play a tic-tac-toe game against an AI.
+
+1. pip3 install -r requirements.txt to install pygame
+2. Run 'python runner.py' to play the game!
+
+(completed as part of CS50 AI course on EdX, not all code is mine)
--- tictactoe.py
+++ tictactoe.py
@@ -4,6 +4,7 @@ Tic Tac Toe Player
 
 import math
 import numpy as np
+import copy
 
 X = "X"
 O = "O"
@@ -37,6 +38,7 @@ def player(board):
     Returns player who has the next turn on a board.
     """
     # raise NotImplementedError
+    board = copy.deepcopy(board)
     num_board = numerical_board(board)
     if np.sum(num_board)==1:
         return O
@@ -47,20 +49,53 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    board = copy.deepcopy(board)
+
+    num_board = numerical_board(board)
+
+    actions = set()
+
+    for row in range(3):
+        for cell in range(3):
+            if num_board[row][cell]==0:
+                actions.add((row,cell))
+
+    if np.all(num_board!=0):
+        return None
+
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    result_board = copy.deepcopy(board)
+
+#     determine what symbol will be placed
+    symbol = player(result_board)
+
+    move = action
+
+    row = move[0]
+    cell = move[1]
+
+    if symbol == 'X':
+        result_board[row][cell]=X
+
+    if symbol == 'O':
+        result_board[row][cell]=O
+
+    return result_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    board = copy.deepcopy(board)
+
     num_board = numerical_board(board)
     #   check rows for winner
     for row in range(3):
@@ -72,7 +107,7 @@ def winner(board):
     for column in range(3):
         if np.sum(num_board.T[column])==3:
             return X
-        if np.sum(num_board[row])==-3:
+        if np.sum(num_board.T[column])==-3:
             return O
 #   check diagonals for winner:
     if np.sum(np.diagonal(num_board))==3:
@@ -92,10 +127,14 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    board = copy.deepcopy(board)
+
+    num_board = numerical_board(board)
+
     if winner(board)==X or winner(board)==O:
         return True
 
-    elif np.any(num_board!=0):
+    elif np.all(num_board!=0):
         return True
 
     else:
@@ -113,9 +152,63 @@ def utility(board):
     if winner(board)==None:
         return 0
 
+# def max_value(board):
+#     if terminal(board):
+#         return utility(board)
+#     v = -np.inf
+#
+#     for action in actions(board):
+#         v = max(v, min_value(result(board,action)))
+#
+#     return action
+#
+# def min_value(board):
+#     if terminal(board):
+#         return utility(board)
+#     v = np.inf
+#
+#     for action in actions(board):
+#         v = min(v, max_value(result(board,action)))
+#         move =
+#
+#     return action
+def max_value(board):
+    if terminal(board):
+        return (None, utility(board))
+    value = float("-inf")
+    move = None
+    for action in actions(board):
+        v = min_value(result(board, action))[1]
+        if v > value:
+            value = v
+            move = action
+    return (move, value)
+
+def min_value(board):
+    if terminal(board):
+        return (None, utility(board))
+    value = float("inf")
+    move = None
+    for action in actions(board):
+        v = max_value(result(board, action))[1]
+        if v < value:
+            value = v
+            move = action
+    return (move, value)
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    turn = player(board)
+
+    if terminal(board):
+        return none
+
+    if board == initial_state():
+        return (np.random.randint(0,3), np.random.randint(0,3))
+
+    if turn==X:
+        return max_value(board)[0]
+    if turn==O:
+        return min_value(board)[0]
